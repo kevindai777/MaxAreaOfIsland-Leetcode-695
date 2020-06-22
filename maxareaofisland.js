91016c32c68f770fa01bc4b56d81529f14464324
kevindai777 <kevin.dai.2451@gmail.com>
2020-06-22 08:56:40 -0400

    edits to solution

--- maxareaofisland.js
+++ maxareaofisland.js
@@ -1,19 +1,18 @@
 //Objective is to find the largest area of an 'island' in a 2-D array
 //An island is defined by 1's grouped up
 
-//O(n) solution that includes a dfs through the 2-D array
-
 let grid = 
-[
-[0,0,1,0,0,0,0,1,0,0,0,0,0],
+[[0,0,1,0,0,0,0,1,0,0,0,0,0],
 [0,0,0,0,0,0,0,1,1,1,0,0,0],
 [0,1,1,0,1,0,0,0,0,0,0,0,0],
 [0,1,0,0,1,1,0,0,1,0,1,0,0],
 [0,1,0,0,1,1,0,0,1,1,1,0,0],
 [0,0,0,0,0,0,0,0,0,0,1,0,0],
 [0,0,0,0,0,0,0,1,1,1,0,0,0],
-[0,0,0,0,0,0,0,1,1,0,0,0,0]
-]
+[0,0,0,0,0,0,0,1,1,0,0,0,0]]
+
+
+//O(n) solution that includes a dfs through the 2-D array
 
 let area = 0
 
@@ -23,17 +22,19 @@ for (let i = 0; i < grid.length; i++) {
     }
 }
 
-//In this dfs function, we first check the boundaries
-//Then, if the boundaries are good, we increase the count of the island size by 1
-//Then we pass this new count into our dfs function to check up, down, right, and left
 function dfs(grid, i, j, count = 0) {
+    //Check the boundaries first
     if (i < 0 || i >= grid.length || j < 0 || j >= grid[i].length || grid[i][j] == 0) {
         return count
     }
+
+    //If the boundaries are good, we increase the count of the island size by 1
     count++
 
+    //Make tile 0 to make sure we don't count it again
     grid[i][j] = 0
     
+    //Then we pass this new count into our dfs function to check up, down, right, and left
     count = dfs(grid, i + 1, j, count)
     count = dfs(grid, i - 1, j, count)
     count = dfs(grid, i, j + 1, count)
